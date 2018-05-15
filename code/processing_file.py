315ea8bad4fe8a0a2058606a1f316d24addbb225
yulianmiao <mintmao@outlook.com>
2018-05-15 16:06:39 +0800

    modified

--- code/processing_file.py
+++ code/processing_file.py
@@ -1,4 +1,18 @@
 import read_file
+from numpy import linalg as la
+import numpy as np
 
 
-read_file.read_files()
+
+print("--------Start SVD----------")
+information = read_file.read_files()
+U, Sigma, Vt = la.svd(np.matrix(information[0]), full_matrices=False)
+p = U
+q = np.dot(np.diag(Sigma), Vt)
+print("The rank of the initial matrix is:", la.matrix_rank(information[0]) )
+print(p.shape)
+print(q.shape)
+
+
+
+
